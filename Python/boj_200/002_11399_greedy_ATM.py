43af54576c5959f986f990a6c06b7b96346b670d
coil <75682926+ilsung12@users.noreply.github.com>
2021-11-12 19:06:35 +0900

    📝 Learn : 11399 - update

--- Python/boj_200/002_11399_greedy_ATM.py
+++ Python/boj_200/002_11399_greedy_ATM.py
@@ -6,10 +6,16 @@ time = list(map(int, input().split()))
 time.sort()
 
 ret = 0
-for i in range(person):
-    count = 0
-    for j in range(0, i + 1):
-        count += time[j]
+# for i in range(person):
+#     count = 0
+#     for j in range(0, i + 1):
+#         count += time[j]
+#     ret += count
+
+count = 0
+for i in time:
+    count += i
     ret += count
+
 
 print(ret)

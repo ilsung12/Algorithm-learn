6ea506fcd7dbe0737cf8c7231659b344ed774f83
ilsung12 <a01041118225@gmail.com>
2021-11-19 11:14:36 +0900

    📝 Learn : 1541_greedy

--- Python/boj_200/001_11047_greedy_coin.py
+++ Python/boj_200/001_11047_greedy_coin.py
@@ -6,14 +6,13 @@ coins = []
 
 for n in range(N):
     coins.append(int(input()))
-coins.sort(reverse=True)
-
+coins.sort(reverse=True) # 큰 값부터 K 에 나누어야 하기 때문 !
 
 cnt = 0
 for i in coins:
-    if K == 0:
+    if K == 0: # break Point
         break
-    cnt += K // i
-    K %= i
+    cnt += K // i # 몫이 카운트가 됨
+    K %= i # 나머지 값을 K로 넣고 다시 돌림 
 
 print(cnt)
--- Python/boj_200/003_1931_greedy_room.py
+++ Python/boj_200/003_1931_greedy_room.py
@@ -19,23 +19,23 @@ time = []
 
 for _ in range(rooms):
   start, end = map(int, input().split())
-  time.append([start, end])
+  time.append([start, end]) 
 
 time = sorted(time, key=lambda a: a[0]) # 시작 시간을 기준으로 오름차순
-print('sorted_1', sorted(time, key=lambda a: a[0]))
+# print('sorted_1', sorted(time, key=lambda a: a[0]))
 time = sorted(time, key=lambda a: a[1]) # 끝나는 시간을 기준으로 다시 오름차순
-print('sorted_2', sorted(time, key=lambda a: a[1]))
-print('time', time)
+# print('sorted_2', sorted(time, key=lambda a: a[1]))
+# print('time', time)
 
 outTime = 0 # 회의의 마지막 시간을 저장할 변수
 conut = 0 # 회의 개수를 저장할 변수
 
 
-for i, j in time:
+for n, k in time:
     
-  if i >= outTime: # 시작시간이 회의의 마지막 시간보다 크거나 같을경우
+  if n >= outTime: # 시작시간이 회의의 마지막 시간보다 크거나 같을경우
     conut += 1
-    outTime = j
-    print('i, j', i, j)
+    outTime = k
+    print('n, k', n, k)
 
 print('cnt', conut)

4df98af1c7b8a7b1747fcf5434855a8e3b4cc26f
ilsung12 <a01041118225@gmail.com>
2021-12-06 11:42:40 +0900

    📝 Learn : codeup - input

---
+++ Python/codeup_basic100/012_input.py
@@ -0,0 +1,7 @@
+
+# 줄을 바꿔 정수(integer) 2개를 입력받아 줄을 바꿔 출력해보자.
+
+n, k = input().split()
+print(n)
+print(k)
+
---
+++ Python/codeup_basic100/013_input.py
@@ -0,0 +1,8 @@
+# 2개의 문자가 줄을 바꿔 입력된다.
+# 순서를 바꿔 한 줄에 한 문자씩 출력한다.
+
+n = chr(input())
+k = chr(input())
+
+print(k)
+print(n)

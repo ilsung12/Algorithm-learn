56d2d268bbb97d87fd4cd59d4c34913df69439ae
ilsung12 <a01041118225@gmail.com>
2021-12-15 17:47:52 +0900

    📝 Repeat : codeup - 6074

--- Python/repeat_wrong/6074_basic.py
+++ Python/repeat_wrong/6074_basic.py
@@ -6,11 +6,11 @@
 
 # a부터 입력한 문자까지 순서대로 공백을 두고 한 줄로 출력한다.
 
-alpabet = str(input())
+alpabet = str(input()) # str 으로 문자를 받는다.
 
-
-#print(ord(alpabet))
-
-
-for i in range(ord('a'), ord(alpabet) + 1):
+# 받은 문자를 ord 메서드로 숫자(idx)로 변환하고,
+# range메서드를 통해 1 부터 입력받은 글자까지 출력
+for i in range(ord('a'), ord(alpabet) + 1):     
     print(chr(i), end=' ')
+
+    

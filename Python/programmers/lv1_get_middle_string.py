ff68443e7b9620be72e6256773ceaea7e67f4e20
ilsung12 <a01041118225@gmail.com>
2021-12-28 17:18:20 +0900

    📝 Learn : Programmers - lv1

---
+++ Python/programmers/lv1_get_middle_string.py
@@ -0,0 +1,14 @@
+# 단어 s의 가운데 글자를 반환하는 함수, 
+# solution을 만들어 보세요. 
+# 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+def solution(s):
+    if len(s) % 2 == 0:
+        answer = int(len(s)/2)
+        return str(s[answer-1]+s[answer])
+
+    if len(s) % 2 == 1:
+        answer = int(len(s)/2)
+        return str(s[answer])
+
+print(solution("qwer"))
